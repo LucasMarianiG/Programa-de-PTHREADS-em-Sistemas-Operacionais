97ae4dd0ea3d6d55ae44ed9ba01ec5599d9f888d
lucas Mariani <lucas2607.gomes@gmail.com>
2025-06-14 16:21:35 -0300

    atualização do codigo, reestruturação do arquivo e implementação da busca serial.

--- Principal.c
+++ Principal.c
@@ -11,40 +11,52 @@
 #include <math.h> 
 
 // Definindo constantes para a matriz e o macrobloqueio
-#define MATRIZ_LINHAS 10000
-#define MATRIZ_COLUNAS 10000
-#define MACROBLOCO_ALTURA 1000
-#define MACROBLOCO_LARGURA 1000
+//#define MATRIZ_LINHAS 10000
+//#define MATRIZ_COLUNAS 10000
+//#define MACROBLOCO_ALTURA 1000
+//#define MACROBLOCO_LARGURA 1000
+#define MATRIZ_LINHAS 9
+#define MATRIZ_COLUNAS 9
+#define MACROBLOCO_ALTURA 3
+#define MACROBLOCO_LARGURA 3
+// Semente para números aleatórios
+#define SEED 42 
+// Máximo número aleatório a ser gerado
+#define MAX_NUM_ALEATORIO 30
+
 
 
 int** matriz;
 
+
+
 void* threadFunc(void*);
+//essa não tem muito o que falar, é apenas a declaração de uma função que verifica se um número é primo ou não : )
+int ehPrimo(int n);
+// Função que percorre a matriz de forma serial e conta os números primos
+void buscaSerial();
+
+void buscaParalelo();
+
+void criaMatriz();
+
+void preencheMatriz();
+
+void imprimeMatriz();
 
 int main(int argc, char* argv[]) {
 	pthread_t thread;
 
-	matriz = malloc(MATRIZ_LINHAS * sizeof(int*)); // Alocando espaço para os ponteiros
-	int* dados = malloc(MATRIZ_LINHAS * MATRIZ_COLUNAS * sizeof(int)); // Alocando todos os elementos
+	// Cria a matriz dinamicamente 
+	criaMatriz();
 
-	if (matriz == NULL || dados == NULL) { 
-		perror("Erro ao alocar memória para matriz");
-		exit(1);
-	}
+	// Preenche a matriz com números aleatórios
+	preencheMatriz(); 
 
-	// Inicializando os ponteiros da matriz para apontar para os dados alocados
-	for (int i = 0; i < MATRIZ_LINHAS; i++) { 
-		matriz[i] = &dados[i * MATRIZ_COLUNAS];
-	}
+	// Imprime a matriz para verificação
+	imprimeMatriz();
 
-	srand(42); // Semente para números aleatórios
-
-	// Preenchendo a matriz com números aleatórios entre 0 e 3200
-	for (int i = 0; i < MATRIZ_LINHAS; i++) {
-		for (int j = 0; j < MATRIZ_COLUNAS; j++) {
-			matriz[i][j] = rand() % 32000;
-		}
-	}
+	buscaSerial();
 
 	if (pthread_create(&thread, NULL, threadFunc, NULL) != 0) {
 		perror("Pthread_create falhou!");
@@ -54,9 +66,80 @@ int main(int argc, char* argv[]) {
 		perror("Pthread_join falhou!");
 		exit(1);
 	}
-	printf("\oi");
+	printf("Print do Main");
 	return 0;
 }
+
+void criaMatriz() {
+	matriz = malloc(MATRIZ_LINHAS * sizeof(int*)); // Alocando espaço para os ponteiros
+	int* dados = malloc(MATRIZ_LINHAS * MATRIZ_COLUNAS * sizeof(int)); // Alocando todos os elementos
+	if (matriz == NULL || dados == NULL) { 
+		perror("Erro ao alocar memória para matriz");
+		exit(1);
+	}
+
+	// Inicializando os ponteiros da matriz para apontar para os dados alocados
+	for (int i = 0; i < MATRIZ_LINHAS; i++) { 
+		matriz[i] = &dados[i * MATRIZ_COLUNAS];
+	}
+}
+
+void preencheMatriz() {
+	srand(SEED); // Semente para números aleatórios
+
+	// Preenchendo a matriz com números aleatórios entre 0 e 32000
+	for (int i = 0; i < MATRIZ_LINHAS; i++) {
+		for (int j = 0; j < MATRIZ_COLUNAS; j++) {
+			matriz[i][j] = rand() % MAX_NUM_ALEATORIO;
+		}
+	}
+}
+
+
+//essa não tem muito o que falar, é apenas a declaração de uma função que verifica se um número é primo ou não : )
+int ehPrimo(int n) {
+	if (n < 2) return 0;
+	int limiteVerificacao = (int)sqrt(n);
+
+
+	for (int i = 2; i <= limiteVerificacao; i++) {
+		if (n % i == 0) {
+			return 0; // não é primo
+		}
+	}
+	return 1; // é primo
+}
+
+// Função que percorre a matriz de forma serial e conta os números primos
+void buscaSerial() {
+	clock_t inicio = clock();
+
+	int totalPrimos = 0;
+
+	for (int i = 0; i < MATRIZ_LINHAS; i++) {
+		for (int j = 0; j < MATRIZ_COLUNAS; j++) {
+			int primo = ehPrimo(matriz[i][j]);
+			totalPrimos+= primo;
+		}
+	}
+
+	clock_t fim = clock();
+	double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+	printf("Busca Serial: %d primos encontrados\n", totalPrimos);
+	printf("Tempo de execução (serial): %.2f segundos\n", tempo);
+}
+
 void* threadFunc(void* nenhum) {
 	printf("Print da Thread\n");
+}
+
+void imprimeMatriz() {
+	printf("Matriz %d x %d:\n \n", MATRIZ_LINHAS, MATRIZ_COLUNAS);
+	for (int i = 0; i < MATRIZ_LINHAS; i++) {
+		for (int j = 0; j < MATRIZ_COLUNAS; j++) {
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
 }
